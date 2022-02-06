2cbd711acec04166a5bcec51e56fc6b8252b7e65
KristiBo <kristina.boroh@gmail.com>
2022-02-06 16:22:44 +0300

    feat: add play/pause to song

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,1 +1,33 @@
-console.log()
+console.log();
+
+const player = document.querySelector('.player');
+const song = document.querySelector('.song');
+const songImg = document.querySelector('.song-img');
+const track = document.querySelector('.track');
+const artist = document.querySelector('.artist');
+const progressLine = document.querySelector('.progress-line');
+const songProgress = document.querySelector('.song-progress');
+const prevBtn = document.querySelector('.prev-btn');
+const playBtn = document.querySelector('.play-btn');
+const playImg = document.querySelector('.play-img');
+const nextBtn = document.querySelector('.next-btn');
+
+function playSong() {
+  player.classList.add('play');
+  playImg.src = `./assets/png/pause.png`;
+  song.play();
+};
+
+function pauseSong() {
+  player.classList.remove('play');
+  playImg.src = `./assets/png/play.png`;
+  song.pause();
+};
+
+playBtn.addEventListener('click', () => { 
+  if (player.classList.contains('play')) {
+    pauseSong();
+  } else {
+    playSong();
+  };
+});
