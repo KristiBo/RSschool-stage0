3d6dc3d580f5b133a161f0e18cd1aa415598d651
KristiBo <kristina.boroh@gmail.com>
2022-02-12 23:14:25 +0300

    feat: add start photos to page

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,1 +1,23 @@
 console.log()
+
+const form = document.querySelector('.search-container');
+const search = document.querySelector('.input-search');
+const photoContainer = document.querySelector('.photo-container');
+const startUrl = 'https://api.unsplash.com/search/photos?query=architecture&per_page=24&orientation=squarish&client_id=Ytg9bYYiHSnGt-M1cFA1mPBnbm2dXkkGJQr9DHIZWzU';
+let searchUrl = 'https://api.unsplash.com/search/photos?per_page=24&orientation=squarish&client_id=Ytg9bYYiHSnGt-M1cFA1mPBnbm2dXkkGJQr9DHIZWzU&query=';
+
+async function getData(url) {
+  const resp = await fetch(url);
+  const data = await resp.json();
+  showData(data);
+  console.log(data);
+};
+
+function showData(data) {
+  data.results.map((el) => {
+    const img = `<img class="photo" src="${el.urls.regular}" alt="image">`;
+    photoContainer.insertAdjacentHTML('afterbegin', img);
+  });
+};
+
+getData(startUrl);
