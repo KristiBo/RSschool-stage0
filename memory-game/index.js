34dd94f9a36da06d2dc87b07c6a123b95fa93da7
KristiBo <kristina.boroh@gmail.com>
2022-02-21 01:10:22 +0300

    feat: add functions start and end game

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,22 +1,35 @@
 console.log();
 
+const startBtn = document.querySelector('.start-btn');
+const text = document.querySelector('.text');
+const cardsContainer = document.querySelector('.cards-container');
 const cards = document.querySelectorAll('.card');
 const stepsCount = document.querySelector('.steps');
+const resultBtn = document.querySelector('.result-btn');
 
 let steps = 0;
 let firstCard;
 let secondCard;
 let spin = false;
 let fieldLocked = false;
+let countMatch = 0;
+
+function startGame () {
+  startBtn.style.display = 'none';
+  text.style.display = 'flex';
+  cardsContainer.style.display = 'grid';
+}
+
+startBtn.addEventListener('click', startGame);
 
 stepsCount.textContent = steps;
 
-(function getRandom() {
+/*(function getRandom() {
   cards.forEach(el => {
     let randomCard = Math.floor(Math.random() * 20);
     el.style.order = randomCard;
   });
-})();
+})();*/
 
 function spinCard(event) {
   if(fieldLocked) return;
@@ -35,23 +48,30 @@ function spinCard(event) {
   };
 };
 
+cards.forEach(el => el.addEventListener('click', spinCard));
+
 function cardsMatch() {
   if(firstCard.dataset.front === secondCard.dataset.front) {
     firstCard.removeEventListener('click', spinCard);
     secondCard.removeEventListener('click', spinCard);
+    countMatch ++;
+    console.log(countMatch);
   } else {
     fieldLocked = true;
-    
+
     setTimeout(() => {
       firstCard.classList.remove('spin');
       secondCard.classList.remove('spin');
-      fieldLocked = false;
       fieldReset();
-    }, 1000);  
+    }, 1000);
   };
   
   steps ++;
   stepsCount.textContent = steps;
+
+  if(countMatch === 10) {
+    endGame();
+  };
 };
 
 function fieldReset() {
@@ -59,4 +79,9 @@ function fieldReset() {
   spin = fieldLocked = false;
 };
 
-cards.forEach(el => el.addEventListener('click', spinCard));
+function endGame() {
+    fieldReset();
+    cards.forEach(el => el.classList.remove('spin'));
+    stepsCount.textContent = 0;
+    console.log('Game over');  
+};
