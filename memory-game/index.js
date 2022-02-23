73d3bbe0bab21bc9ad45a5a8a51178937d37f1ee
KristiBo <kristina.boroh@gmail.com>
2022-02-24 02:25:13 +0300

    feat: add local storage

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,15 +1,11 @@
-console.log(`Доброго дня! 
-По возможности, прошу перепроверить работу в четверг.
-Не успела сделать хранилище.
-Благодарю!
-
-1. Вёрстка +10
+console.log(`1. Вёрстка +10
 2. Логика игры. Карточки, по которым кликнул игрок, переворачиваются согласно правилам игры +10
 3. Игра завершается, когда открыты все карточки +10
 4. По окончанию игры выводится её результат - количество ходов, которые понадобились для завершения игры +10
 5. По клику на карточку – она переворачивается плавно, если пара не совпадает – обе карточки так же плавно переворачиваются рубашкой вверх +10
 6. Дополнительный не предусмотренный в задании функционал, улучшающий качество приложения +10
-(предполагает собственное оригинальное оформление равное или отличающееся в лучшую сторону по сравнению с демо)`);
+(предполагает собственное оригинальное оформление равное или отличающееся в лучшую сторону по сравнению с демо)
+7. Результаты последних 10 игр сохраняются в local storage. Есть таблица рекордов, в которой сохраняются результаты предыдущих 10 игр +10`);
 
 const startBtn = document.querySelector('.start-btn');
 const text = document.querySelector('.text');
@@ -29,6 +25,7 @@ let secondCard;
 let spin = false;
 let fieldLocked = false;
 let countMatch = 0;
+let gameResults = [];
 
 function startGame() {
   startBtn.style.display = 'none';
@@ -88,6 +85,8 @@ function cardsMatch() {
 
   if(countMatch === 10) {
     endGame();
+    let newResult = stepsCount.textContent + ' steps';
+    addResults(newResult);
   };
 };
 
@@ -115,18 +114,49 @@ function endGameReset() {
 function openEndModal() {
   modalEnd.classList.add('open');
   modalEndText.textContent = `Your result is ${steps} steps`;
-}
+};
 
 function openResultModal() {
   resultModal.classList.add('open');
-  //resultText.textContent = `Win with ${steps} steps`;//
-}
+  resultText.textContent = '';
+  getResults();
+};
 
 function closeModal() {
   modalEnd.classList.remove('open');
   resultModal.classList.remove('open');
-}
+};
 
 resultBtn.addEventListener('click', openResultModal);
 
 modalBtn.forEach(el => el.addEventListener('click', closeModal));
+
+function getResults() {
+  if (localStorage.getItem('result')) {
+    gameResults = JSON.parse(localStorage.getItem('result'));
+    showResults(gameResults);
+  };
+};
+
+function showResults(gameResults) {
+  if (gameResults.length > 0) {
+    gameResults.forEach((el) => {
+      let result = document.createElement('li');
+      result.classList.add('result');
+      result.textContent = el;
+      resultText.appendChild(result);
+    });
+  };
+};
+
+function addResults(newResult) {
+  if (gameResults.length === 10) {
+    gameResults.shift();
+    gameResults.push(newResult);
+  } else {
+    gameResults.push(newResult);
+  };
+
+  localStorage.setItem('result', JSON.stringify(gameResults));
+  showResults(gameResults);
+};
